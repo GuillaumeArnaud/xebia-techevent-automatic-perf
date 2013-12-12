afedb28cd26982f603980b9928c177b1258a22ba
Guillaume Arnaud <garnaud@xebia.fr>
2013-12-12 02:02:09 +0100

    many many

---
+++ ansible/controller.sh
@@ -0,0 +1,1 @@
+ssh -i ssh/gatling-gar.pem ec2-user@controller-$1.aws.xebiatechevent.info -o UserKnownHostsFile=/dev/null -o StrictHostKeychecking=no
---
+++ ansible/webapp.sh
@@ -0,0 +1,1 @@
+ssh -i ssh/gatling-gar.pem ec2-user@webapp-$1.aws.xebiatechevent.info -o UserKnownHostsFile=/dev/null -o StrictHostKeychecking=no
