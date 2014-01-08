5b42a673b6f756fbd9efb5a0127f4af8673e7715
Guillaume Arnaud <garnaud@xebia.fr>
2014-01-08 11:48:48 +0100

    touche finale

--- ansible/files/deploy.sh
+++ ansible/files/deploy.sh
@@ -3,5 +3,5 @@ CATALINA_BASE="/home/ec2-user/catalina_base"
 ssh -oStrictHostKeyChecking=no -i /home/jenkins/.ssh/gatling-gar.pem ec2-user@$TOMCAT_IP '/home/ec2-user/shutdown.sh'
 ssh -oStrictHostKeyChecking=no -i /home/jenkins/.ssh/gatling-gar.pem ec2-user@$TOMCAT_IP "rm -rf $CATALINA_BASE/webapps/*;rm -rf $CATALINA_BASE/temp/*"
 scp -oStrictHostKeyChecking=no -i /home/jenkins/.ssh/gatling-gar.pem /home/jenkins/.m2/repository/com/xebia/tinyapp/0.0.1-SNAPSHOT/tinyapp-0.0.1-SNAPSHOT.war ec2-user@$TOMCAT_IP:$CATALINA_BASE/webapps/tinyapp.war
-ssh -oStrictHostKeyChecking=no -i /home/jenkins/.ssh/gatling-gar.pem ec2-user@$TOMCAT_IP '/opt/apache-tomcat/tomcat8/bin/startup.sh'
+ssh -oStrictHostKeyChecking=no -i /home/jenkins/.ssh/gatling-gar.pem ec2-user@$TOMCAT_IP '/home/ec2-user/startup.sh'
 echo "war deployed"
--- ansible/run.sh
+++ ansible/run.sh
@@ -1,4 +1,4 @@
 echo "StrictHostKeyChecking no" >> ~/.ssh/config
 export AWS_ACCESS_KEY=AKIAJJWGCHMN5DLV5O3Q
 export AWS_SECRET_KEY=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
-ansible-playbook -vvv ec2-graphite.yaml --extra-vars "user=ec2-user id=$1" --private-key ssh/gatling-gar.pem
+ansible-playbook -vvvv ec2.yml --extra-vars "user=ec2-user id=$1" --private-key ssh/gatling-gar.pem 
